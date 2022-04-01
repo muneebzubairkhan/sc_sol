eba8c23d665d73fd32a09f77173b30fc09e02306
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-01 18:00:37 +0500

    added calculator code, test working

--- programs/sc_sol/src/lib.rs
+++ programs/sc_sol/src/lib.rs
@@ -3,8 +3,13 @@ use anchor_lang::prelude::*;
 declare_id!("4DzTzA5yGGX9bF7fpScPmRKLEMfxqaLUuECBsMbH1kEt");
 
 #[program]
-pub mod sc_sol {
+pub mod sol_sc {
     use super::*;
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
+        Ok(())
+    }
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
     }
@@ -12,3 +17,19 @@ pub mod sc_sol {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 264)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64,
+}
